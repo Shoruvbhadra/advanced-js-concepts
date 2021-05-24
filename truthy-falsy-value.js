b82dc43e24240f75c89d363d3bd3672d9640682d
Shoruvbhadra <67528362+Shoruvbhadra@users.noreply.github.com>
2021-05-24 17:58:11 +0600

    true-false value in js

---
+++ truthy-falsy-value.js
@@ -0,0 +1,15 @@
+// falsy values in js : 
+
+// 0;
+// '';
+// undefined ;
+// NaN;
+// null;
+// false keyword; 
+
+let Name = false;
+if (Name) {
+    console.log("true");
+}else{
+    console.log("false");
+}
